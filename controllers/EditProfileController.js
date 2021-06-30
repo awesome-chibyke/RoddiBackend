f001db83fb70462c117cecd1d6b1d9380745d52b
uke <ukemike4@gmail.com>
2021-06-30 18:29:27 +0100

    Added validations to edit profile controller

--- controllers/EditProfileController.js
+++ controllers/EditProfileController.js
@@ -2,16 +2,47 @@ const responseObject = require("./ViewController");
 const authData = require("../helpers/AuthenticateLogin");
 const ErrorHandler = require("../helpers/ErrorHandler");
 const User = require("../model/User");
+const validator = require("../helpers/validator");
 
 class EditController {
   constructor() {
     this.responseObject = new responseObject();
     this.User = new User();
     this.now = new Date();
+    this.errorMessage = "";
+    this.errorStatus = true;
+  }
+
+  valdateFunction(req, ValidationRule) {
+    validator(req.body, ValidationRule, {}, (err, status) => {
+      if (status === false) {
+        this.errorMessage = err;
+      }
+      this.errorStatus = status;
+    });
   }
 
   async edit(req, res) {
     try {
+      //validation
+      let validationRule = {
+        first_name: "required|string",
+        last_name: "required|string",
+        middle_name: "required|string",
+        address: "required|string",
+        state: "required|string",
+        country: "required|string",
+        city: "required|string",
+        zip_code: "required|numeric",
+      };
+
+      this.valdateFunction(req, validationRule);
+      if (this.errorStatus === false) {
+        this.responseObject.setStatus(false);
+        this.responseObject.setMessage(this.errorMessage.errors);
+        return res.json(this.responseObject.sendToView());
+      }
+
       //authenticate user
       let userObject = await authData(req);
       userObject = userObject.user;
@@ -41,7 +72,6 @@ class EditController {
       res.json(this.responseObject.sendToView());
     }
   }
-
 }
 
 module.exports = EditController;
